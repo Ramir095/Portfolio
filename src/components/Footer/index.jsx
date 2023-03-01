5c84e5b76e48ac1af7bb9e968f5bac7595ccb651
Ramiro Aduviri <Kevin_95_ah@hotmail.com.ar>
2023-02-28 22:35:20 -0300

    feature: responsive

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className=" w-20 h-screen flex flex-col items-center justify-center absolute right-0">
+    <div className=" hidden w-20 h-screen md:flex flex-col items-center justify-center absolute right-0">
       <div className="fixed z-[100] text-[#9da5ac]">
         <div
           className="py-5 cursor-pointer hover:text-[#F2F2F2] transition-all duration-300 rounded-xl"
--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -4,9 +4,24 @@ const Skills = () => {
   return (
     <div className="w-full h-screen bg-[#2E3140] text-white flex justify-center items-center">
       <div className="text-center">
-        <h1>Skills</h1>
-        <h1 className="text-8xl">Ramiro Aduviri</h1>
-        <h2 className="text-5xl">Full Stack Developer</h2>
+        <h1>Kevin Ramiro Aduviri</h1>
+
+        <div className="flex items-center justify-center bg-red-900">
+          <img 
+            src="https://icongr.am/fontawesome/map-marker.svg?size=128&color=F2F2F2" alt="location"
+            className="w-8 h-8" />
+          <span>Buenos Aires, Argentina</span>
+        </div>
+
+        <div className="flex items-center justify-center ">
+          <img src="https://icongr.am/fontawesome/envelope.svg?size=128&color=F2F2F2" alt="envelope" className="w-8 h-7 mr-1.5"/>
+          <a href="mailto:contacto@rama-dev.com" >contacto@rama-dev.com</a>
+          <button onClick={() => {window.navigator.clipboard.writeText("contacto@rama-dev.com")}} className="w-4 h-4 ml-2">
+            <img src="https://icongr.am/fontawesome/copy.svg?size=128&color=F2F2F2" />
+          </button>
+        </div>
+
+        <h1>Perfil Profesional</h1>
       </div>
     </div>
   );
