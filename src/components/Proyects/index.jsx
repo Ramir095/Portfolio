a2fe8e7312545e28ce6b3896681991702dc4f24c
Ramiro Aduviri <Kevin_95_ah@hotmail.com.ar>
2023-03-03 09:59:29 -0300

    chore: typography

--- src/components/Proyects/index.jsx
+++ src/components/Proyects/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Portfolio = () => {
+const Proyects = () => {
   return (
     <div className="w-full h-screen bg-[#2E3140] text-white flex justify-center items-center">
       <div className="text-center">
-        <h1>Portfolio</h1>
+        <h1>Proyects</h1>
         <h1 className="text-8xl">Ramiro Aduviri</h1>
         <h2 className="text-5xl">Full Stack Developer</h2>
       </div>
@@ -12,4 +12,4 @@ const Portfolio = () => {
   )
 }
 
-export default Portfolio
+export default Proyects
