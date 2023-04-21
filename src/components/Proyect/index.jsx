510701245e0049eb95c32698d1ea2c5ccfecd24f
Ramiro Aduviri <Kevin_95_ah@hotmail.com.ar>
2023-04-20 22:11:39 -0300

    fix: home update

--- src/components/Proyect/index.jsx
+++ src/components/Proyect/index.jsx
@@ -1,12 +1,18 @@
 import { Button } from "../Button";
 
-const Proyect = ({ img, title, description, buttons }) => {
+const Proyect = ({ img, title, description, buttons, left, background }) => {
   return (
     <>
       {
-        <div className="md:h-[25rem] md:flex bg-[#2E3140]">
-          <img src={img} alt="" className="md:w-1/2" />
-          <div className="w-full md:flex md:flex-col md:justify-center md:items-center space-y-5 md:px-10">
+        <div className={`md:h-[25rem] md:w-full md:flex bg-[#2E3140] ${!left ? 'md:flex-row-reverse' : '' }`}>
+          <figure className="md:w-1/2 flex items-center justify-center relative overflow-hidden z-10">
+            <div className='bg-[#252734f2] w-full h-full absolute' />
+            <div className='-z-10 absolute'>
+              <img className='w-[70%] -rotate-45' src={background} alt='Fondo de pantalla' />
+            </div>
+            <img src={img} alt={`Imagen del proyecto ${title}`} className='md:w-[80%] md:h-[70%] z-10' />
+          </figure>
+          <div className="md:w-1/2 md:flex md:flex-col md:justify-center md:items-center space-y-5 md:px-10">
             <span className="md:text-4xl font-['Cormorant']">
               {title}
             </span>
