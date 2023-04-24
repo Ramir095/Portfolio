daf0ddea30328daf668dde3aa97d6b962f469301
Ramiro Aduviri <Kevin_95_ah@hotmail.com.ar>
2023-04-24 12:33:48 -0300

    fix: update

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -3,9 +3,9 @@ import { skills } from "../../constants/skills";
 
 const Skills = () => {
   return (
-    <div className="w-full h-screen bg-[#2E3140] text-white flex justify-center items-center font-['Cormorant'] flex-col md:space-y-24 2xl:space-y-16" id="skills">
+    <div className="w-full md:h-screen my-10 space-y-10 md:my-0 bg-[#2E3140] text-white flex justify-center items-center font-['Cormorant'] flex-col md:space-y-24 2xl:space-y-16" id="skills">
      <h1 className="text-4xl md:text-6xl" >Mis Habilidades</h1>
-     <div className="flex justify-between w-[90%]">
+     <div className="flex space-y-5 md:space-y-0 flex-col md:flex-row md:justify-between w-[90%] flex-wrap">
        {
         skills && skills.map((s, i) => (
           <CardSkills key={i} data={s} />
--- src/pages/Proyects/index.jsx
+++ src/pages/Proyects/index.jsx
@@ -9,7 +9,7 @@ const Proyects = () => {
           <h1 className="text-4xl md:text-6xl font-['Cormorant'] 2xl:mb-[4.8rem]" >Mis proyectos</h1>
          {
           proyect && proyect?.map(p => (
-            <Proyect img={p.img} title={p.title} description={p.description} buttons={p.buttons} left={p.left} background={p.background} />
+            <Proyect img={p.img} imgPhone={p.imgPhone} title={p.title} description={p.description} buttons={p.buttons} left={p.left} background={p.background} />
           ))
          }
         </div>
